705282dac88bad07f3b6d36d9dc3d67246ea15f0
khodzha <khodzha.sh@gmail.com>
2012-08-24 19:32:51 +0300

    rewritten using libs

--- src/Percolation.java
+++ src/Percolation.java
@@ -2,25 +2,15 @@ import java.lang.IndexOutOfBoundsException;
 
 public class Percolation {
     private int size;
-    private int[] array;
     private boolean[] opened;
-    private int[] sz;
     private boolean percolates = false;
+    private WeightedQuickUnionUF data;
     
     public Percolation(int N) {
         size = N;
         // N*N + 1 - beginning of percolation system
         // N*N + 2 - end of percolation system
-        array = new int[N * N + 2];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = -i;
-        }
-        array[N * N] = N * N;
-        array[N * N + 1] = N * N + 1;
-        sz = new int[N * N + 2];
-        for (int i = 0; i < sz.length; i++) {
-            sz[i] = 1;
-        }
+        data = new WeightedQuickUnionUF(N*N + 2);
         opened = new boolean[N * N + 2];
         for (int i = 0; i < opened.length; i++) {
             opened[i] = false;
@@ -33,26 +23,25 @@ public class Percolation {
         int index = convertToIndex(i, j);
         int c;
         if (!opened[index]) {
-            array[index] = index;
             opened[index] = true;
         }
         if (i == 1)
-            union(index, size * size);
+            data.union(index, size * size);
         if (i < size && isOpen(i + 1, j)) {
             c = convertToIndex(i + 1, j);
-            union(index, c);
+            data.union(index, c);
         }
         if (i > 1 && isOpen(i - 1, j)) {
             c = convertToIndex(i - 1, j);
-            union(index, c);
+            data.union(index, c);
         }
         if (j < size && isOpen(i, j + 1)) {
             c = convertToIndex(i, j + 1);
-            union(index, c);
+            data.union(index, c);
         }
         if (j > 1 && isOpen(i, j - 1)) {
             c = convertToIndex(i, j - 1);
-            union(index, c);
+            data.union(index, c);
         }
 
     }
@@ -63,13 +52,13 @@ public class Percolation {
 
     public boolean isFull(int i, int j) {
         int index = convertToIndex(i, j);
-        return connected(index, size * size);
+        return data.connected(index, size * size);
     }
 
     public boolean percolates() {
         if (!percolates) {
             for (int j = 1; j <= size; j++) {
-                if(connected(convertToIndex(size, j), size*size)) {
+                if(data.connected(convertToIndex(size, j), size*size)) {
                     percolates = true;
                     break;
                 }
@@ -82,35 +71,5 @@ public class Percolation {
         if (i > size || j > size || i < 1 || j < 1)
             throw new IndexOutOfBoundsException();
         return (i - 1) * size + j - 1;
-    }
-
-    private int root(int index) {
-        int i = index;
-        while (i >= 0 && i != array[i])
-            i = array[i];
-        return i;
-    }
-
-    private void union(int i, int j) {
-        int p = root(i);
-        int q = root(j);
-        if (sz[p] < sz[j]) {
-            array[p] = q;
-            sz[q] += sz[p];
-        } else {
-            array[q] = p;
-            sz[p] += sz[q];
-        }
-    }
-
-    private boolean connected(int i, int j) {
-        if (!opened[i] || !opened[j])
-            return false;
-        return root(i) == root(j);
-    }
-
-    public static void main(String[] args) {
-        Percolation q = new Percolation(5);
-        System.out.println(q.percolates());
     }
 }
